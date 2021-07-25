4c1214607ed6c6eca42c14b3bf74e59212618c82
danielefzanini <danielezanini500@hotmail.com>
2021-07-25 16:22:05 -0300

    first commit

---
+++ src/view/Home/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 80%;
+  margin: auto;
+  
+  .Logo{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+   
+  }
+  section {
+    height: 100vh;
+    width: 100%;
+    background: #e2e2;
+    display: flex;
+    justify-content: space-around;
+    .product-content{
+      display: grid;
+      text-align: center;
+      height: 300px;
+      background: #fff;
+      border-radius: 12px;
+      padding: 12px;
+    }
+  }
+`
